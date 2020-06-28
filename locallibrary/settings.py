545b0da4804fb7b5a0751b389b629c1560d768a4
JoeF2020 <gurmoney@gmail.com>
2020-06-29 01:55:53 +0300

    Update dev server url

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -20,15 +20,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '76f@+e%37*t-oqo651zvlj&@##02x)*+r^!xmv@5jj0)+@k4i&'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '76f@+e%37*t-oqo651zvlj&@##02x)*+r^!xmv@5jj0)+@k4i&')
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = TRUE
-DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', False) )
 
 
-ALLOWED_HOSTS = ['ancient-hollows-53538.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['https://ancient-hollows-53538.herokuapp.com','127.0.0.1']
 
 
 # Application definition
